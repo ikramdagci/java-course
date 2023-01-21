22364be7d49a594d27c6ef779ac8a411f1cc6b9b
Yunus Emre Çiftçi <118928402+yunus-emre-ciftci@users.noreply.github.com>
2023-01-21 14:08:56 +0300

    Homework

--- src/com/course/reusability/domain/people/Student.java
+++ src/com/course/reusability/domain/people/Student.java
@@ -2,34 +2,26 @@ package com.course.reusability.domain.people;
 
 public class Student extends Person {
 
-    private String no;
+    private int no;
     private int grade;
     private int lockerNo;
 
-    public Student(String fullName, String tckn, String address, char gender, String no, int grade, int lockerNo) {
+    private static int counter = 1;
+
+
+
+    public Student(String fullName, String tckn, String address, char gender, int grade, int lockerNo) {
         super(fullName, tckn, address, gender);
-        this.no = no;
+        this.no = counter++;
         this.grade = grade;
         this.lockerNo = lockerNo;
     }
 
-
-
+    public int getNo() {
+        return no;
+    }
 
     public void printInfo() {
         System.out.println("Full Name : "+ fullName + ", Gender : " + gender + ", Address : " + address +  ", No : " + no + ", Grade : " + grade);
-    }
-
-    public void test0(){
-        test1(this);
-        //test2(super); compile error
-    }
-
-    public void test1(Student student){
-        student.printInfo();
-    }
-
-    public void test2(Person person){
-        person.printInfo();
     }
 }
